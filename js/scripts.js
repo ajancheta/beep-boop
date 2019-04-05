4c08f1daa472d7cb195c7baf297a035ee9e50bc7
AJ Ancheta <ashleyjancheta@gmail.com>
2019-04-05 15:10:52 -0700

    copy over code from original file on personal computer

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,47 @@
 // Business Interface logic
 
+function isNumber(input) {
+  if (isNaN(input)) {
+    alert("Please enter a number that I can read.")
+  } else {
+    return true;
+  }
+}
 
+var execute = function convertNumToWords(numInput) {
+  var output = [];
+  var userName = $("#nameInput").val();
+  for (var i=0; i <=numInput; i++) {
+    if ([i].toString().includes("1")) {
+      output.push(" Beep");
+    } else if ([i].toString().includes("2")) {
+      output.push(" Boop");
+    } else if ([i].toString().includes("3")) {
+      output.push(" I'm sorry," + userName + ". I'm afraid I can't do that");
+    } else {
+      output.push(" " + [i]);
+    }
+  }
+    return output;
+};
 
 // User Interface logic
-$(document).ready(function(event) {
+$(document).ready(function(){
   $("#formOne").submit(function(event){
     event.preventDefault();
-    var number = parseInt($("#numberInput").val());
+    var nameInput = $("input#name").val();
+    $(".name").text(nameInput);
+    $("#formTwo").fadeToggle();
+    $("#formOne").hide();
+    $(".welcomemsg").hide();
+    $("")
 
+  });
+
+  $("#formTwo").submit(function(event){
+    event.preventDefault();
+    var userInput = $("input#userInput").val();
+    isNumber(userInput);
+    $("h3").text(execute(userInput));
   });
 });
